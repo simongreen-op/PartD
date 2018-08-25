4ddcc0723f913e5317ced997bb58c37d43a47f7d
simongreen-op <simongreen3@myopenpolytechnic.ac.nz>
2018-08-25 19:18:27 +1200

    Add favourite colour

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Print information about myself
 		System.out.println("Hi, my name is Simon");
+		System.out.println("My favourite colour is Black");
 	}
 
 }
